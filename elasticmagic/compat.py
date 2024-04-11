a47ba4fb035641227c4f16daae39a7eaf81a80e5
v.frolkin <v.frolkin@smartweb.com.ua>
2024-04-11 15:59:18 +0300

    remove compat functionality

--- elasticmagic/compat.py
+++ elasticmagic/compat.py
@@ -1,18 +1,6 @@
-try:
-    from collections.abc import Iterable, Mapping  # noqa:F401
-except ImportError:
-    from collections import Iterable, Mapping  # noqa:F401
-
-
-text_type = str
-string_types = (str,)
-binary_type = str
-int_types = (int,)
-
-
 def force_unicode(value):
     """
-    Forces a bytestring to become a Unicode string.
+    Forces bytes to become a string.
     """
     if isinstance(value, bytes):
         value = value.decode('utf-8', errors='replace')
@@ -20,16 +8,3 @@ def force_unicode(value):
         value = str(value)
 
     return value
-
-
-def with_metaclass(meta, *bases):
-    class metaclass(meta):
-        __call__ = type.__call__
-        __init__ = type.__init__
-
-        def __new__(cls, name, this_bases, d):
-            if this_bases is None:
-                return type.__new__(cls, name, (), d)
-            return meta(name, bases, d)
-
-    return metaclass('temporary_class', None, {})
